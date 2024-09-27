74aff0cd3348e541b762c4370e1d90d73a205d58
Nguyễn Kiều Anh Quân <168531710+CharleSs0933@users.noreply.github.com>
2024-09-28 01:59:42 +0700

    Tao JWT Access Token

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './modules/users/users.module';
-import { LikesModule } from './modules/likes/likes.module';
+import { AppController } from '@/app.controller';
+import { AppService } from '@/app.service';
+import { UsersModule } from '@/modules/users/users.module';
+import { LikesModule } from '@/modules/likes/likes.module';
 
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { MenuItemOptionsModule } from './modules/menu.item.options/menu.item.options.module';
-import { MenuItemsModule } from './modules/menu.items/menu.items.module';
-import { MenusModule } from './modules/menus/menus.module';
-import { OrderDetailModule } from './modules/order.detail/order.detail.module';
-import { OrdersModule } from './modules/orders/orders.module';
-import { RestaurantsModule } from './modules/restaurants/restaurants.module';
-import { ReviewsModule } from './modules/reviews/reviews.module';
+import { MenuItemOptionsModule } from '@/modules/menu.item.options/menu.item.options.module';
+import { MenuItemsModule } from '@/modules/menu.items/menu.items.module';
+import { MenusModule } from '@/modules/menus/menus.module';
+import { OrderDetailModule } from '@/modules/order.detail/order.detail.module';
+import { OrdersModule } from '@/modules/orders/orders.module';
+import { RestaurantsModule } from '@/modules/restaurants/restaurants.module';
+import { ReviewsModule } from '@/modules/reviews/reviews.module';
+import { AuthModule } from '@/auth/auth.module';
 
 @Module({
   imports: [
@@ -25,6 +26,7 @@ import { ReviewsModule } from './modules/reviews/reviews.module';
     OrdersModule,
     RestaurantsModule,
     ReviewsModule,
+    AuthModule,
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
